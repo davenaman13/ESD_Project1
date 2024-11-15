a8ad5fe8061319836a370f123c12b279bbdd66e4
Naman131 <davenaman13@gmail.com>
2024-11-15 16:57:24 +0530

    Initialized Project

---
+++ src/test/java/com/namandave/yummyapp/YummyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namandave.yummyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
